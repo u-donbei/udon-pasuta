9afbff867d2c73328c576d0dd84d5d0dc3d6fafd
Yu WATANABE <yu_w_main@yahoo.co.jp>
2024-09-07 11:50:51 +0900

    [add] Animation.javaの追加
    アニメーション可能なことを表す「Animation.java」を追加

---
+++ src/main/java/jp/u_donbei/udon_pasuta/object/Animation.java
@@ -0,0 +1,17 @@
+package jp.u_donbei.udon_pasuta.object;
+
+/**
+ * アニメーションできることを表す。
+ */
+public interface Animation {
+	/**
+	 * 次のテクスチャに変更する。
+	 */
+	void nextTexture();
+
+	/**
+	 * テクスチャを変更する。
+	 * @param id テクスチャID。IDは各クラスが関連付けます。
+	 */
+	void changeTexture(int id);
+}
