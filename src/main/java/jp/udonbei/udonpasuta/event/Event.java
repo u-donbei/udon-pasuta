6e200ab9e2b5649cf9b55c3d553e64c5a0e659d2
Yu WATANABE <yu_w_main@yahoo.co.jp>
2024-11-13 17:02:22 +0900

    [add] イベントに引数を渡す機能を追加
    イベントの発動時に引数を渡す機能を追加しました。

--- src/main/java/jp/udonbei/udonpasuta/event/Event.java
+++ src/main/java/jp/udonbei/udonpasuta/event/Event.java
@@ -9,10 +9,12 @@
 package jp.udonbei.udonpasuta.event;
 
 import jp.udonbei.udonpasuta.control.TextPanel;
+import jp.udonbei.udonpasuta.function.TriConsumer;
 import jp.udonbei.udonpasuta.object.character.Udonbei;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -20,6 +22,7 @@ import java.util.function.Predicate;
 /**
  * イベント(トリガー処理)を表す。
  */
+@Slf4j
 @Builder
 @Getter
 @EqualsAndHashCode
@@ -34,13 +37,18 @@ public class Event {
      * トリガーを引いたときに実行される処理
      */
     @NonNull
-    private final BiConsumer<Udonbei, TextPanel> run;
+    private final TriConsumer<Udonbei, TextPanel, Map<String, Object>> run;
 
     /**
      * 実行するかどうかの判断に使うPredicate
      */
-    @Builder.Default()
-    private final Predicate<Udonbei> judgement = args -> true;
+    @Builder.Default
+    private final Predicate<Udonbei> judgement = udonbei -> true;
+    /**
+     * 実行の際に必要としている引数
+     */
+    @Builder.Default
+    private final Map<String, Class<?>> requiredArgs = new HashMap<>();
 
     /**
      * 条件を満たした場合は{@link #run}の処理を実行する。
@@ -49,8 +57,43 @@ public class Event {
      * @param textPanel 表示用のTextPanel
      */
     public void run(Udonbei udonbei, TextPanel textPanel) {
+        run(udonbei, textPanel, new HashMap<>());
+    }
+
+    /**
+     * 条件を満たした場合は{@link #run}の処理を実行する。
+     *
+     * @param udonbei 判断に使うプレイヤー
+     * @param textPanel 表示用のTextPanel
+     * @param args 渡す引数
+     */
+    public void run(Udonbei udonbei, TextPanel textPanel, Map<String, Object> args) {
         if (judgement.test(udonbei)) {
-            run.accept(udonbei, textPanel);
+            if (!checkArgs(args)) {
+                throw new IllegalArgumentException("argsが不正です。正しい値は" + requiredArgs + "です。");
+            }
+            run.accept(udonbei, textPanel, args);
         }
+    }
+
+    /**
+     * 引数の数、型などが{@link #requiredArgs}と一致しているか確認する
+     * @param args 確認する引数
+     * @return 確認した結果
+     */
+    private boolean checkArgs(Map<String, Object> args) {
+        if (requiredArgs.size() != args.size()) {
+            log.warn("args.size() is invalid");
+            return false;
+        }
+        for (String key : requiredArgs.keySet()) {
+            if (!args.containsKey(key)) {
+                return false;
+            }
+            if (!(args.get(key).getClass() == requiredArgs.get(key))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/jp/udonbei/udonpasuta/event/EventManager.java
+++ src/main/java/jp/udonbei/udonpasuta/event/EventManager.java
@@ -10,17 +10,20 @@ package jp.udonbei.udonpasuta.event;
 
 import jp.udonbei.udonpasuta.control.TextPanel;
 import jp.udonbei.udonpasuta.object.character.Udonbei;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 
 /**
  * イベントを監視し、実行する。
  */
+@Slf4j
 public final class EventManager {
     /**
      * 登録されたイベントを格納する。
      */
     private final List<Event> events = new ArrayList<>();
+    private final Map<Integer, Map<String, Object>> args = new HashMap<>();
 
     /**
      * イベントを登録する。
@@ -30,6 +33,15 @@ public final class EventManager {
     public int register(Event event) {
         events.add(event);
         return events.indexOf(event);
+    }
+
+    /**
+     * イベントに渡す引数を設定する。
+     * @param index イベントのインデックス
+     * @param args 渡す引数
+     */
+    public void setArgs(int index, Map<String, Object> args) {
+        this.args.put(index, args);
     }
 
     /**
@@ -50,6 +62,13 @@ public final class EventManager {
      * @param textPanel 表示用のTextPanel
      */
     public void runEvents(Udonbei player, TextPanel textPanel) {
-        events.forEach(event -> event.run(player, textPanel));
+        events.forEach(event -> {
+            if (args.containsKey(events.indexOf(event))) {
+                log.debug("args={}", args.get(events.indexOf(event)));
+                event.run(player, textPanel, args.get(events.indexOf(event)));
+            } else {
+                event.run(player, textPanel);
+            }
+        });
     }
 }
